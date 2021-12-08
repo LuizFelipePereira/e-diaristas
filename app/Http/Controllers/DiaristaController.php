d874ccf99133c04938cd98499fc60d0d09d44888
felipewned@hotmail.com <felipewned@hotmail.com>
2021-12-08 07:26:00 -0300

    Organizacao em subviews / Create e Update das Diaristas

--- app/Http/Controllers/DiaristaController.php
+++ app/Http/Controllers/DiaristaController.php
@@ -26,9 +26,37 @@ class DiaristaController extends Controller
     public function store(Request $request)
     {
         $dados = $request->except('_token');
-        $dados['foto_usuario'] = $request->foto_usuario->store('public');
+
+        if ($request->file('foto_usuario') == null) {
+            $dados['foto_usuario'] = "";
+        } else {
+            $dados['foto_usuario'] = $request->foto_usuario->store('public');
+        }
+
+
 
         Diarista::create($dados);
+
+        return redirect()->route('diaristas.index');
+    }
+
+    public function edit(int $id)
+    {
+        $diarista = Diarista::findOrFail($id);
+
+        return view('edit', ['diarista' => $diarista]);
+    }
+    public function update(int $id, Request $request)
+    {
+        $diarista = Diarista::findOrFail($id);
+
+        $dados = $request->except(['_token', '_method']);
+
+        if ($request->hasFile('foto_usuario')) {
+            $dados['foto_usuario'] = $request->foto_usuario->store('public');
+        }
+
+        $diarista->update($dados);
 
         return redirect()->route('diaristas.index');
     }
