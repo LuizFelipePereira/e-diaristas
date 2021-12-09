496e6b7a93bb5d9f9ac9c0a82a6719b44ef02182
felipewned@hotmail.com <felipewned@hotmail.com>
2021-12-09 06:35:32 -0300

    Utilizacao de Mascara nos campos e Documentação do Controller

--- app/Http/Controllers/DiaristaController.php
+++ app/Http/Controllers/DiaristaController.php
@@ -8,7 +8,9 @@ use App\Models\Diarista;
 class DiaristaController extends Controller
 {
 
-
+    /**
+     * Lista as Diaristas
+     */
     public function index()
     {
 
@@ -18,11 +20,17 @@ class DiaristaController extends Controller
         ]);
     }
 
+    /**
+     * Mostra o Formulário de Criação
+     */
     public function create()
     {
         return view('create');
     }
 
+    /**
+     * Cria uma diarista no banco de dados
+     */
     public function store(Request $request)
     {
         $dados = $request->except('_token');
@@ -33,24 +41,37 @@ class DiaristaController extends Controller
             $dados['foto_usuario'] = $request->foto_usuario->store('public');
         }
 
-
-
+        $dados['cpf'] = str_replace(['.', '-'], '', $dados['cpf']);
+        $dados['cep'] = str_replace('-', '', $dados['cep']);
+        $dados['telefone'] = str_replace(['(', ')', ' ', '-'], '', $dados['telefone']);
         Diarista::create($dados);
+
 
         return redirect()->route('diaristas.index');
     }
 
+    /**
+     * Mostra o Formulário de edição populado
+     */
     public function edit(int $id)
     {
         $diarista = Diarista::findOrFail($id);
 
         return view('edit', ['diarista' => $diarista]);
     }
+
+    /**
+     * Atualiza uma diarista no banco de dados
+     */
     public function update(int $id, Request $request)
     {
         $diarista = Diarista::findOrFail($id);
 
         $dados = $request->except(['_token', '_method']);
+
+        $dados['cpf'] = str_replace(['.', '-'], '', $dados['cpf']);
+        $dados['cep'] = str_replace('-', '', $dados['cep']);
+        $dados['telefone'] = str_replace(['(', ')', ' ', '-'], '', $dados['telefone']);
 
         if ($request->hasFile('foto_usuario')) {
             $dados['foto_usuario'] = $request->foto_usuario->store('public');
@@ -58,6 +79,16 @@ class DiaristaController extends Controller
 
         $diarista->update($dados);
 
+        return redirect()->route('diaristas.index');
+    }
+
+    /**
+     * Apaga uma diarista no banco de dados
+     */
+    public function destroy(int $id)
+    {
+        $diarista = Diarista::findOrFail($id);
+        $diarista->delete();
         return redirect()->route('diaristas.index');
     }
 }
